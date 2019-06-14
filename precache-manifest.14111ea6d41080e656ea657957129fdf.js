feb1060633fe2c4707b81838043f975373d6d3ea
Michael Lisboa <michael.lisboa@gmail.com>
2019-06-14 10:04:03 +0700

    Updates

---
+++ precache-manifest.14111ea6d41080e656ea657957129fdf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf16a3a1d237b370fceb31163a495137",
+    "url": "/react-portfolio-site/index.html"
+  },
+  {
+    "revision": "4d9b944b25d280531cd2",
+    "url": "/react-portfolio-site/static/css/main.c199f1cb.chunk.css"
+  },
+  {
+    "revision": "1d37caba333d810d89c0",
+    "url": "/react-portfolio-site/static/js/2.96f46020.chunk.js"
+  },
+  {
+    "revision": "4d9b944b25d280531cd2",
+    "url": "/react-portfolio-site/static/js/main.e22fec49.chunk.js"
+  },
+  {
+    "revision": "cf17da3606906085921d",
+    "url": "/react-portfolio-site/static/js/runtime~main.47e1dff8.js"
+  },
+  {
+    "revision": "3658d8d298d3420a1167cc5934bc3ca7",
+    "url": "/react-portfolio-site/static/media/astro-logo.3658d8d2.svg"
+  },
+  {
+    "revision": "f3500fb88c2827937c22a6a4b24cdb38",
+    "url": "/react-portfolio-site/static/media/creative-strategy-c.f3500fb8.png"
+  },
+  {
+    "revision": "2eb003b532086643f7588c4a4a02c238",
+    "url": "/react-portfolio-site/static/media/creative-strategy-s.2eb003b5.png"
+  },
+  {
+    "revision": "9f012ac4d95a7d77aa21781e481b005a",
+    "url": "/react-portfolio-site/static/media/creative-technology-c.9f012ac4.png"
+  },
+  {
+    "revision": "98723074eafbaa98ac030c6254f37d9a",
+    "url": "/react-portfolio-site/static/media/creative-technology-s.98723074.png"
+  },
+  {
+    "revision": "6b7e9013d2431299e464d31b93bad982",
+    "url": "/react-portfolio-site/static/media/insead.6b7e9013.png"
+  },
+  {
+    "revision": "2f534f871df385e8ade66b254e2deba9",
+    "url": "/react-portfolio-site/static/media/oreo.2f534f87.png"
+  },
+  {
+    "revision": "bd0dc0d5ff65811e744d4b10fc4d5596",
+    "url": "/react-portfolio-site/static/media/sony.bd0dc0d5.png"
+  },
+  {
+    "revision": "40bd351a724b986e4868217d4fdd40f4",
+    "url": "/react-portfolio-site/static/media/user-experience-c.40bd351a.png"
+  },
+  {
+    "revision": "63a43a6a3001e32847a4f62d0c49fa32",
+    "url": "/react-portfolio-site/static/media/user-experience-s.63a43a6a.png"
+  }
+]);
