17c38f30d8981683fa088c5c563a003c493ec683
Michael <michael@MacBook-Pro.local>
2019-07-09 08:51:28 +0700

    sw issues

--- src/sw_2.js
+++ src/sw_2.js
@@ -32,15 +32,12 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      // const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw_2.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
