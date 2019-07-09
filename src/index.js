184b9384017a6974705952d9a56927aca1bf35fb
Michael <michael@MacBook-Pro.local>
2019-07-09 08:46:13 +0700

    sw issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/Main.css";
 import App from "./App";
-import * as serviceWorker from "./sw.js";
+import * as serviceWorker from "./service-worker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -32,8 +32,8 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
