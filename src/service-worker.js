016ecde3141f325508c8c258fd66093bfacbfa36
Michael <michael@MacBook-Pro.local>
2019-07-09 08:54:15 +0700

    sw issues

--- src/service-worker.js
+++ src/service-worker.js
@@ -32,8 +32,8 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      const swUrl = `${process.env.PUBLIC_URL}/sw_2.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/sw_2.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
