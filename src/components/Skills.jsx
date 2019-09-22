820a55e9dba3e208f8841c33ae844bb831b1de3e
Michael <michael@pixt.us>
2019-09-22 13:19:14 +0700

    minor changes to components. clean up

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import SkillsContent from "./presentation/SkillsContent.jsx";
 import '../css/Skills.css';
 
 
-class Skills extends Component {
-    render () {
-        return (
-            <section className="skills-section uk-section uk-section-large uk-section-default
-                uk-padding-remove-vertical uk-padding-medium-bottom@s">
-                <div className="uk-text-center uk-container uk-container-small">
-                    <h4 className="uk-heading-line text-background uk-margin-small-top uk-margin-large-bottom">
-                        <span>I'm a Technocreativeologist.</span>
-                    </h4>
-                    <p className="uk-margin-remove-bottom uk-margin-medium-bottom@s">
-                        See my portfolio <NavLink className="text-background text-background-indigo
-                        uk-link-reset uk-link uk-nowrap" to={'/portfolio'}>
-                        for case studies
-                    </NavLink>, or check out my <NavLink
-                        className="text-background text-background-indigo
-                        uk-link-reset uk-link uk-nowrap" to={'/rates'}>
-                        freelance rates</NavLink>.</p>
-                </div>
-                <SkillsContent />
-            </section>
-        );
-    }
-}
+const Skills = () =>
+    <section className="skills-section uk-section uk-section-large uk-section-default
+        uk-padding-remove-vertical uk-padding-medium-bottom@s">
+        <div className="uk-text-center uk-container uk-container-small">
+            <h4 className="uk-heading-line text-background uk-margin-small-top uk-margin-large-bottom">
+                <span>I'm a Technocreativeologist.</span>
+            </h4>
+            <p className="uk-margin-remove-bottom uk-margin-medium-bottom@s">
+                See my portfolio <NavLink className="text-background text-background-indigo
+                uk-link-reset uk-link uk-nowrap" to={'/portfolio'}>
+                for case studies
+            </NavLink>, or check out my <NavLink
+                className="text-background text-background-indigo
+                uk-link-reset uk-link uk-nowrap" to={'/rates'}>
+                freelance rates</NavLink>.</p>
+        </div>
+        <SkillsContent />
+    </section>
 
 export default Skills;
